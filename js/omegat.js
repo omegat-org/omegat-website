05d76e1b38353291353fc77f5aaeba8d00199572
briacp <briacp@gmail.com>
2017-03-27 14:22:39 +0200

    Initial import

---
+++ js/omegat.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+//  $(".slideanim").each(function(){
+//    var pos = $(this).offset().top;
+//
+//    var winTop = $(window).scrollTop();
+//    if (pos < winTop + 600) {
+//      $(this).addClass("slide");
+//    }
+//  });
+});
+
